ec64ef2e7c8e629c1079946671ba1b247df05f4b
splincode <omaxphp@yandex.ru>
2023-11-09 17:47:36 +0300

    chore: update

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,23 +1,17 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideClientHydration} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
 import {TuiDialogModule, TuiRootModule} from '@taiga-ui/core';
 import {TuiPushModule} from '@taiga-ui/kit';
-import {NG_EVENT_PLUGINS} from '@tinkoff/ng-event-plugins';
 
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        NG_EVENT_PLUGINS,
+        provideAnimations(),
         provideRouter(routes),
         provideClientHydration(),
-        importProvidersFrom(
-            BrowserAnimationsModule,
-            TuiRootModule,
-            TuiDialogModule,
-            TuiPushModule,
-        ),
+        importProvidersFrom(TuiRootModule, TuiDialogModule, TuiPushModule),
     ],
 };
