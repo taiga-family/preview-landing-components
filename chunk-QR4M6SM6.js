2b3b394cffd82b79feb16c29847b137b746fe46c
splincode <omaxphp@yandex.ru>
2024-11-27 09:29:23 +0000

    Deploying to gh-pages from @ taiga-family/preview-landing-components@072f5ef92c91c82deda5bc549bd5d79da9f7e002 🚀

---
+++ chunk-QR4M6SM6.js
@@ -0,0 +1,2 @@
+import{$a as e,E as t}from"./chunk-JVX3OKP3.js";var o=e.create({name:"tableRow",addOptions(){return{HTMLAttributes:{}}},content:"(tableCell | tableHeader)*",tableRole:"row",parseHTML(){return[{tag:"tr"}]},renderHTML({HTMLAttributes:r}){return["tr",t(this.options.HTMLAttributes,r),0]}});export{o as TableRow,o as default};
+//# sourceMappingURL=chunk-QR4M6SM6.js.map
