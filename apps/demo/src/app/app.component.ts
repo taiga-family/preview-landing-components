072f5ef92c91c82deda5bc549bd5d79da9f7e002
Максим Иванов <omaxphp@yandex.ru>
2024-11-27 12:28:13 +0300

    feat: upgrade to Angular 19 (#715)

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {TuiRoot} from '@taiga-ui/core';
-import {TuiEditor, TuiEditorSocket} from '@taiga-ui/editor';
 
 @Component({
     standalone: true,
-    selector: 'app',
-    imports: [RouterOutlet, TuiEditor, TuiEditorSocket, TuiRoot],
+    selector: 'app-root',
+    imports: [RouterOutlet, TuiRoot],
     templateUrl: './app.component.html',
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
