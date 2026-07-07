d22a5f426a81d0a39550033517343c5675651ec7
splincode <omaxphp@yandex.ru>
2026-07-07 13:11:38 +0300

    chore: migration taiga ui

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,17 +1,14 @@
 import {
     type ApplicationConfig,
     importProvidersFrom,
-    INJECTOR,
-    type Injector,
     provideZonelessChangeDetection,
 } from '@angular/core';
 import {provideClientHydration} from '@angular/platform-browser';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
+import {provideTaiga} from '@taiga-ui/core';
 import {NgDompurifySanitizer} from '@taiga-ui/dompurify';
-import {TUI_EDITOR_DEFAULT_EXTENSIONS, TUI_EDITOR_EXTENSIONS} from '@taiga-ui/editor';
-import {NG_EVENT_PLUGINS} from '@taiga-ui/event-plugins';
-import {TUI_SANITIZER} from '@taiga-ui/legacy';
+import {provideTuiEditor, TUI_EDITOR_SANITIZER} from '@taiga-ui/editor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -26,20 +23,22 @@ export const appConfig: ApplicationConfig = {
             },
         ]),
         importProvidersFrom(),
+        provideTuiEditor({
+            // You can disable these plugins
+            // if you don't need them
+            image: true,
+            iframe: true,
+            video: true,
+            source: true,
+            audio: true,
+            details: true,
+            detailsSummary: true,
+            detailsContent: true,
+        }),
         {
-            provide: TUI_EDITOR_EXTENSIONS,
-            deps: [INJECTOR],
-            useFactory: (injector: Injector) => [
-                ...TUI_EDITOR_DEFAULT_EXTENSIONS,
-                import('@taiga-ui/editor').then(({tuiCreateImageEditorExtension}) =>
-                    tuiCreateImageEditorExtension({injector}),
-                ),
-            ],
-        },
-        {
-            provide: TUI_SANITIZER,
+            provide: TUI_EDITOR_SANITIZER,
             useClass: NgDompurifySanitizer,
         },
-        NG_EVENT_PLUGINS,
+        provideTaiga(),
     ],
 };
